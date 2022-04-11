19f801a38a2c318d6a63c0b1131e4123fc9d2ef9
porkfrnd <90595891+porkfrnd@users.noreply.github.com>
2022-04-11 08:51:10 +0545

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,41 @@
+import os
+import ctypes
+import requests
+import random
+import string
+import time
+from discord import Webhook, RequestsWebhookAdapter
+webhook_input = input("webhook link: ")
+webhook = Webhook.from_url(webhook_input, adapter=RequestsWebhookAdapter())
+
+num = int(input('Input How Many Codes to Generate and Check: '))
+
+with open("Nitro Codes.txt", "w", encoding='utf-8') as file:
+    print("Your nitro codes are being generated, be patient if you entered the high number!")
+
+    start = time.time()
+
+    for i in range(num):
+        code = "".join(random.choices(
+            string.ascii_uppercase + string.digits + string.ascii_lowercase,
+            k = 19
+        ))
+
+        file.write(f"https://discord.gift/{code}\n")
+
+    print(f"Generated {num} codes | Time taken: {time.time() - start}\n")
+
+with open("Nitro Codes.txt") as file:
+    for line in file.readlines():
+        nitro = line.strip("\n")
+
+        url = "https://discordapp.com/api/v6/entitlements/gift-codes/" + nitro + "?with_application=false&with_subscription_plan=true"
+
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            print(f" Valid | {nitro} ")
+            webhook.send(content=f"@everyone nitro code: {nitro}")
+            break
+        else:
+            print(f" Invalid | {nitro} ") 
